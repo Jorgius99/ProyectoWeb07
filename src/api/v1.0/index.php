ebe683dea5f14a369657db51aff12f6abebd06cb
daniel <dmesmun@epsg.upv.es>
2021-05-14 06:42:53 +0200

    gitflow-feature-stash: enlanube

--- src/api/v1.0/index.php
+++ src/api/v1.0/index.php
@@ -9,9 +9,9 @@ if($metodo ==='POST') {
 
 
     $serverNombre = "localhost";
-    $userNombre = "root";
-    $password = "";
-    $dbNombre = "proyectoweb007";
+    $userNombre = "dmesmun_dmesmun";
+    $password = "Proyectoweb_007";
+    $dbNombre = "dmesmun_proyectoweb007";
     $conn = mysqli_connect($serverNombre, $userNombre, $password, $dbNombre);
     if (!$conn) {
         http_response_code(500);

29b46cc9e81607bf39bf73f3a776c3baa18c327c
Carlos <cipiqui@epsg.upv.es>
2021-05-17 17:59:23 +0200

    JSyPHP_Contacto

---
+++ src/api/v1.0/contacto.js
@@ -0,0 +1,23 @@
+
+console.log(document.querySelector("form"))
+
+document.querySelector("form").addEventListener("submit", function (event){
+    console.log(event);
+    event.preventDefault();
+    let dataContacto = new FormData(event.target)  ;
+
+
+    fetch( '../src/api/v1.0/index.php', {
+        method:"POST",
+        body:dataContacto
+
+    }).then(function (respuesta){
+        if(respuesta.ok){
+            return respuesta.json();
+        }
+        else{
+            document.getElementById("output").textContent = "Por favor, complete el formulario";
+        }
+    })
+
+});

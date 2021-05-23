0fbbc415a1a4e35e036678132a8685a59ad11192
Carlos <cipiqui@epsg.upv.es>
2021-05-23 11:19:18 +0200

    contactomodifeid

--- src/api/v1.0/contacto.php
+++ src/api/v1.0/contacto.php
@@ -2,6 +2,7 @@
 <?php
 
 $metodo = $_SERVER['REQUEST_METHOD'];
+session_start();
 
 if($metodo ==='POST') {
 
@@ -14,13 +15,20 @@ if($metodo ==='POST') {
     $userNombre = "root";
     $password = "";
     $dbNombre = "proyectoweb007";
-
     $conn = mysqli_connect($serverNombre, $userNombre, $password, $dbNombre);
     if (!$conn){
         http_response_code(500);
         die("Error: " . mysqli_connect_error());
     }
-    $sql = "INSERT INTO `contacto` (`Nombre`, `Apellido`, `correo`, `motivo`) VALUES ('$Nombre' ,'$Apellido', '$correo','$motivo' )";
-    $result = mysqli_query($conn, $sql);
 
+    $sql = "INSERT INTO `contacto` (`Nombre`, `Apellido`, `correo`, `motivo`) VALUES ('$Nombre' ,'$Apellido', '$correo','$motivo' )";
+    $result = mysqli_query($conn, $sql);//
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "Formulario rellenado de forma correcta";
+    } else {
+        http_response_code(401);
+        die();
+    }
 }
+

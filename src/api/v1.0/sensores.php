e6d280e68a087c5aa8718bc9de091ed591c9d146
Ignacio <iyusazn@epsg.upv.es>
2021-05-24 18:14:44 +0200

    gitflow-feature-stash: marinMaricon

---
+++ src/api/v1.0/sensores.php
@@ -0,0 +1,32 @@
+<?php
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+session_start();
+$idParcela=$_SESSION["idParcela"];
+if($metodo ==='GET') {
+    $serverNombre = "localhost";
+    $userNombre = "root";
+    $password = "";
+    $dbNombre = "proyectoweb007";
+    $conn = mysqli_connect($serverNombre, $userNombre, $password, $dbNombre);
+    if (!$conn) {
+        http_response_code(500);
+        die("Error: " . mysqli_connect_error());
+    }
+    $sql = "SELECT * FROM `sensores` WHERE `idParcela`='$idParcela'";
+    $result = mysqli_query($conn, $sql);//
+    $resultado=array();
+    $i=0;
+    while ($fila = mysqli_fetch_array($result)) {
+
+        $respuesta = [];
+        $respuesta["idSensor"] = $fila ["idSensor"];
+        $respuesta["coordenadaX"] = $fila ["coordenadaX"];
+        $respuesta["coordenadaY"] = $fila ["coordenadaY"];
+        $resultado[$i]=$respuesta;
+        $i++;
+
+    }
+    echo json_encode($resultado);
+
+}

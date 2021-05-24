c032167f7e7f298932f6787417145e6595b7c3d6
Jorge Marín-Roig Zozaya <jmarzoz@epsg.upv.es>
2021-05-25 01:11:18 +0200

    gitflow-feature-stash: mapas

--- src/api/v1.0/mapas.php
+++ src/api/v1.0/mapas.php
@@ -2,33 +2,38 @@
 $metodo = $_SERVER['REQUEST_METHOD'];
 
 session_start();
-$idUser=$_SESSION["id"];
-if($metodo ==='GET') {
+$idUser = $_SESSION["id"];
+if ($metodo === 'GET') {
 
 
-$serverNombre = "localhost";
-$userNombre = "root";
-$password = "";
-$dbNombre = "proyectoweb007";
-$conn = mysqli_connect($serverNombre, $userNombre, $password, $dbNombre);
-if (!$conn) {
-http_response_code(500);
-die("Error: " . mysqli_connect_error());
-}
-$sql = "SELECT * FROM `parcela` WHERE `idUsuario`='$idUser'";
-$result = mysqli_query($conn, $sql);//
-    $resultado=array();
-    $i=0;
-while ($fila = mysqli_fetch_array($result)) {
+    $serverNombre = "localhost";
+    $userNombre = "root";
+    $password = "";
+    $dbNombre = "proyectoweb007";
+    $conn = mysqli_connect($serverNombre, $userNombre, $password, $dbNombre);
+    if (!$conn) {
+        http_response_code(500);
+        die("Error: " . mysqli_connect_error());
+    }
+    $sql = "SELECT * FROM `parcela` WHERE `idUsuario`='$idUser'";
+    $result = mysqli_query($conn, $sql);//
+    $resultado = array();
+    $i = 0;
+    while ($fila = mysqli_fetch_array($result)) {
 
-$respuesta = [];
-$respuesta["idParcela"] = $fila ["idParcela"];
-$respuesta["coordenadaX"] = $fila ["coordenadaX"];
-$respuesta["coordenadaY"] = $fila ["coordenadaY"];
-$resultado[$i]=$respuesta;
-$i++;
+        $respuesta = [];
+        $respuesta["idParcela"] = $fila ["idParcela"];
+        $respuesta["coordenadaX"] = $fila ["coordenadaX"];
+        $respuesta["coordenadaY"] = $fila ["coordenadaY"];
+        $resultado[$i] = $respuesta;
+        $i++;
+        $_SESSION["Parcelas"]=$respuesta["idParcela"];
 
-}
+    }
+
+
+
     echo json_encode($resultado);
+
 
 }

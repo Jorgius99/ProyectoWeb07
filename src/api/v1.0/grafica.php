cfc8329dadbf531a7d621fc40cb53a3ba08a084e
Ignacio <iyusazn@epsg.upv.es>
2021-05-25 18:19:26 +0200

    gitflow-feature-stash: cosas

---
+++ src/api/v1.0/grafica.php
@@ -0,0 +1,37 @@
+<?php
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+session_start();
+$idSensor = $_SESSION["id"];
+
+if ($metodo === 'GET') {
+    $serverNombre = "localhost";
+    $userNombre = "root";
+    $password = "";
+    $dbNombre = "proyectoweb007";
+    $conn = mysqli_connect($serverNombre, $userNombre, $password, $dbNombre);
+    if (!$conn) {
+        http_response_code(500);
+        die("Error: " . mysqli_connect_error());
+    }
+    $sql = "SELECT * FROM `mediciones` WHERE `idSensor`='$idSensor'";
+    $result = mysqli_query($conn, $sql);//
+    $resultado = array();
+    $i = 0;
+    while ($fila = mysqli_fetch_array($result)) {
+
+        $respuesta = [];
+        $respuesta["idSensor"] = $fila ["idSensor"];
+        $respuesta["temperatura"] = $fila ["temperatura"];
+        $respuesta["humedad"] = $fila ["humedad"];
+        $respuesta["salinidad"] = $fila ["salinidad"];
+        $respuesta["luminosidad"] = $fila ["luminosidad"];
+        $respuesta["hora"] = $fila ["hora"];
+        $resultado[$i] = $respuesta;
+        $i++;
+    }
+    echo json_encode($resultado);
+
+
+}
